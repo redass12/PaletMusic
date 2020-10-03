aec90c6ddd559301e1a03741282186f3e81e11cb
redass12 <redataraoui1@gmail.com>
2020-10-04 01:03:50 +0200

    Updates

---
+++ precache-manifest.22cfea626c68cd744dfe8b51d4ea0ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5bd85446fc434cd2393e2abf7ba6048",
+    "url": "/PaletMusic/index.html"
+  },
+  {
+    "revision": "856bf1ed1ece4f710468",
+    "url": "/PaletMusic/static/css/main.1fcaaad6.chunk.css"
+  },
+  {
+    "revision": "c0508d68d76907c412eb",
+    "url": "/PaletMusic/static/js/2.cfa82f13.chunk.js"
+  },
+  {
+    "revision": "b28590a4d5a036be9947256ff9f1f35b",
+    "url": "/PaletMusic/static/js/2.cfa82f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856bf1ed1ece4f710468",
+    "url": "/PaletMusic/static/js/main.602141fa.chunk.js"
+  },
+  {
+    "revision": "824d7146a559817fa2e0",
+    "url": "/PaletMusic/static/js/runtime-main.c40b53f2.js"
+  }
+]);
